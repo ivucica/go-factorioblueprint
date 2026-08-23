0c8761babbf040bba17aed18a3ef4cdbf7230c65
Ivan Vučica <ivan@vucica.net>
2026-08-23 18:27:29 +0100

    read_blueprint, write_blueprint: Add docstrings.

--- read_blueprint/doc.go
+++ read_blueprint/doc.go
@@ -1,1 +1,4 @@
+// Package read_blueprint allows reading a base64-encoded zlib'bed JSON representing a blueprint or a blueprint book.
+//
+// Learn more at https://wiki.factorio.com/Blueprint_string_format
 package read_blueprint // badc0de.net/pkg/factorioblueprint/read_blueprint
--- write_blueprint/doc.go
+++ write_blueprint/doc.go
@@ -1,1 +1,4 @@
+// Package write_blueprint allows writing a base64-encoded gzipped JSON representing a Factorio blueprint or a blueprint book.
+//
+// Learn more at https://wiki.factorio.com/Blueprint_string_format
 package write_blueprint // badc0de.net/pkg/factorioblueprint/write_blueprint
